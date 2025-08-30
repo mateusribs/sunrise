7809ea4987741a9bcdbca9be5a55e527ace88340
mateusribs <mateus.ribe.sva@gmail.com>
2025-08-30 12:32:39 -0300

    infra: create moods models and migration to create tables

--- backend/src/infrastructure/database/orm.py
+++ backend/src/infrastructure/database/orm.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column, registry
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, registry, relationship
 
 table_registry = registry()
 
@@ -9,6 +9,7 @@ table_registry = registry()
 @table_registry.mapped_as_dataclass
 class UserModel:
     __tablename__ = 'users'
+
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True)
@@ -21,3 +22,61 @@ class UserModel:
     )
     is_admin: Mapped[bool] = mapped_column(default=False)
     is_active: Mapped[bool] = mapped_column(default=True)
+
+    moods: Mapped[list['MoodModel']] = relationship(
+        init=False,
+        cascade='all, delete-orphan',
+        lazy='selectin',
+    )
+
+
+@table_registry.mapped_as_dataclass
+class MoodModel:
+    __tablename__ = 'moods'
+
+    user_id: Mapped[str] = mapped_column(ForeignKey('users.id'))
+    visual_scale: Mapped[int]
+    description: Mapped[str]
+    id: Mapped[str] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(default=func.now(), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        default=func.now(), server_default=func.now(), onupdate=func.now()
+    )
+
+    associated_emotions: Mapped[list['AssociatedEmotionsModel']] = relationship(
+        init=False,
+        cascade='all, delete-orphan',
+        lazy='selectin',
+    )
+    triggers: Mapped[list['EmotionalTriggerModel']] = relationship(
+        init=False,
+        cascade='all, delete-orphan',
+        lazy='selectin',
+    )
+
+
+@table_registry.mapped_as_dataclass
+class AssociatedEmotionsModel:
+    __tablename__ = 'associated_emotions'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    mood_id: Mapped[str] = mapped_column(ForeignKey('moods.id'))
+    name: Mapped[str]
+    intensity: Mapped[int]
+    created_at: Mapped[datetime] = mapped_column(default=func.now(), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        default=func.now(), server_default=func.now(), onupdate=func.now()
+    )
+
+
+@table_registry.mapped_as_dataclass
+class EmotionalTriggerModel:
+    __tablename__ = 'emotional_triggers'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    mood_id: Mapped[str] = mapped_column(ForeignKey('moods.id'))
+    name: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(default=func.now(), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        default=func.now(), server_default=func.now(), onupdate=func.now()
+    )
