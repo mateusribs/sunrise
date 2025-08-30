c3dcfe6f4f11b715c7f9d504bec9584a6271f362
mateusribs <mateus.ribe.sva@gmail.com>
2025-08-30 09:47:34 -0300

    test: create unit tests for mood entity

---
+++ backend/tests/domain/test_mood_entity.py
@@ -0,0 +1,80 @@
+import pytest
+from pydantic_core import ValidationError
+
+from src.domain.entities import AssociatedEmotion, Mood, RegistryType, VisualScale
+from src.domain.exceptions.mood_exceptions import InvalidEmotionIntensityError
+
+
+def test_create_valid_mood():
+    mood = Mood(
+        id='1',
+        registry_type=RegistryType.DAILY,
+        visual_scale=VisualScale.HAPPY,
+        associated_emotions=[AssociatedEmotion.JOY],
+        emotions_intensity={AssociatedEmotion.JOY: 5},
+        triggers=['Had a great day!'],
+        description='Feeling joyful and content.',
+    )
+    assert mood.id == '1'
+    assert mood.registry_type == RegistryType.DAILY
+    assert mood.visual_scale == VisualScale.HAPPY
+    assert mood.associated_emotions == [AssociatedEmotion.JOY]
+    assert mood.emotions_intensity == {AssociatedEmotion.JOY: 5}
+    assert mood.triggers == ['Had a great day!']
+    assert mood.description == 'Feeling joyful and content.'
+
+
+def test_create_invalid_registry_type():
+    with pytest.raises(ValidationError):
+        Mood(
+            id='1',
+            registry_type='invalid_type',
+            visual_scale=VisualScale.HAPPY,
+            associated_emotions=[AssociatedEmotion.JOY],
+            emotions_intensity={AssociatedEmotion.JOY: 5},
+        )
+
+
+def test_create_invalid_visual_scale():
+    with pytest.raises(ValidationError):
+        Mood(
+            id='1',
+            registry_type=RegistryType.DAILY,
+            visual_scale='invalid_scale',
+            associated_emotions=[AssociatedEmotion.JOY],
+            emotions_intensity={AssociatedEmotion.JOY: 5},
+        )
+
+
+def test_create_invalid_associated_emotions():
+    with pytest.raises(ValidationError):
+        Mood(
+            id='1',
+            registry_type=RegistryType.DAILY,
+            visual_scale=VisualScale.HAPPY,
+            associated_emotions=['invalid_emotion'],
+            emotions_intensity={AssociatedEmotion.JOY: 5},
+        )
+
+
+def test_create_invalid_emotions_intensity_key():
+    with pytest.raises(ValidationError):
+        Mood(
+            id='1',
+            registry_type=RegistryType.DAILY,
+            visual_scale=VisualScale.HAPPY,
+            associated_emotions=[AssociatedEmotion.JOY],
+            emotions_intensity={'invalid_emotion': 5},
+        )
+
+
+@pytest.mark.parametrize('intensity', [0, 11, -1, 100])
+def test_create_invalid_emotions_intensity_value(intensity):
+    with pytest.raises(InvalidEmotionIntensityError):
+        Mood(
+            id='1',
+            registry_type=RegistryType.DAILY,
+            visual_scale=VisualScale.HAPPY,
+            associated_emotions=[AssociatedEmotion.JOY],
+            emotions_intensity={AssociatedEmotion.JOY: intensity},
+        )
